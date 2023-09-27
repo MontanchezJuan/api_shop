b5ef1351c10f50f1bbba5999905497ab71591f6c
Santiago Silva <silvaguarnizo825@gmail.com>
2023-09-26 19:51:02 -0500

    models created Client, Order, Product, ProductOrder

---
+++ src/main/java/com/shop/shop/models/Order.java
@@ -0,0 +1,68 @@
+package com.shop.shop.models;
+
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document()
+public class Order {
+    @Id
+    private String _id;
+    private String date;
+    private String paymentMethod;
+    private String address;
+
+    @DBRef
+    private Client client;
+
+    public Order(String date, String paymentMethod, String address) {
+        this.date = date;
+        this.paymentMethod = paymentMethod;
+        this.address = address;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setEmail(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+}
